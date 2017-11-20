ccb1630d865bf499b370d5438eef3d30b1961f87
Jerome <>
2017-11-20 17:29:17 +0800

    add postTest demo

--- spring-boot-example/src/test/java/com/jerome/RestTemplateTest.java
+++ spring-boot-example/src/test/java/com/jerome/RestTemplateTest.java
@@ -2,8 +2,13 @@ package com.jerome;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -18,9 +23,57 @@ public class RestTemplateTest {
 
     @Test
     public void getTest() {
-        String url = "http://www.baidu.com";
-        String result = restTemplate.getForObject(url, String.class);
-        log.info("result = {}", result);
+        String url = "http://192.168.2.201:8081/api/authenticate";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("accept","application/x.agent.v1+json");
+        headers.add("authorization","Basic emhpZGE6MTIzMTIz");
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
+
+        ResponseEntity<AuthDto>
+                response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, AuthDto.class, headers);
+        log.info("result = {}", response);
+        log.info("result = {}", response.getBody());
+
     }
+
+
+    @Test
+    public void postTest() {
+        /*
+        AgentReduceSuncionRequestDto requestDto) {
+        log.info("reduceSuncoin, requestDto = {}", requestDto);
+
+        String url = apiUrl + "/api/agent/pay";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("accept", standardsTree);
+        headers.add("authorization", "Bearer " + getToken());
+        headers.add("content-type", "application/json");
+
+        HttpEntity<AgentReduceSuncionRequestDto> requestEntity =
+                new HttpEntity<>(requestDto, headers);
+
+        ResponseEntity<AgentReduceSuncoinResponseDto> response = agentRestTemplate
+                .exchange(url, HttpMethod.POST, requestEntity, AgentReduceSuncoinResponseDto.class,
+                        headers);
+        log.info("result = {}", response);
+
+        if (response.getBody() == null || response.getBody().getData() == null) {
+            throw new GeneralException("get agent is null");
+        }
+
+        return response.getBody();
+        */
+    }
+
+
+
+
+
+}
+
+@Data
+class AuthDto {
+    private String token;
 
 }
